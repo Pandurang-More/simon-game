b9362ab350dbec6da3ab79faefac8fc762b0b319
Pandurang <morepandurang264@gmail.com>
2025-07-18 23:21:51 +0530

    updated js

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-let gameSeq=[];
-let userSeq=[];
+document.addEventListener("DOMContentLoaded", function () {
+  let gameSeq = [];
+  let userSeq = [];
 
-let started=false;
-let level=0;
-let h2=document.querySelector("h2")
+  let btns = ["yellow", "red", "purple", "green"];
+  let started = false;
+  let level = 0;
 
-document.addEventListener("keypress",function(){
-    if(started==false){
-        console.log("Game is started");
-        started=true;
-        levelUP();
-         }   
-});
+  let h2 = document.querySelector("h2");
 
+  document.addEventListener("keydown", function () {
+    if (started == false) {
+      console.log("Game is started");
+      started = true;
+      levelUP();
+    }
+  });
 
-function levelUP(){
+  function btnFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+      btn.classList.remove("flash");
+    }, 250);
+  }
+
+  function levelUP() {
     level++;
-    h2.innerText=`level ${level}`;
+    h2.innerText = `Level ${level}`;
 
+    let randIdx = Math.floor(Math.random() * btns.length);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    console.log(randIdx);
+    console.log(randColor);
+    console.log(randBtn);
+    btnFlash(randBtn);
+  }
 
-}
+  function btnPress() {
+    console.log("Button Pressed");
+  }
+
+  let allBtns = document.querySelectorAll(".btn");
+  for (let btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+  }
+});
